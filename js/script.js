7c42db815db27fc775cacd92a838349ad40d469b
GioGreco <grecogiorgio.97@gmail.com>
2022-10-26 14:55:32 +0200

    add test square

--- js/script.js
+++ js/script.js
@@ -5,7 +5,17 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            message : 'Hello Vue!'
+            message : 'Hello Vue!',
+            textRed : 'red',
+            numArray: [23, 45, 54],
+        }
+    },
+    methods: {
+        createSquare(){
+            const appVue = document.getElementById('app');
+            const square = document.createElement('div');
+            square.className = 'square';
+            appVue.append(square);
         }
     }
 }).mount('#app')
